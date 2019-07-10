4c74cf750ac69cd21ad09f688fc5312350d9e537
Mrhairui <chenwentao_622@163.com>
2019-07-10 23:06:12 +0800

    tensorflow

---
+++ temp.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+NUM_LABELS = 10
+
+CONV1_DEEP = 32
+CONV1_SIZE = 5
+
+CONV2_DEEP = 64
+CONV2_SIZE = 5
+
+FC_SIZE = 512
+
+
+def inference(input_tensor, train, regularizer):
+    with tf.variable_scope('layer1_conv1'):
+        conv1_weights = tf.get_variable('weight', [CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEEP], initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv1_biases = tf.get_variable('bias', [CONV1_DEEP], initializer=tf.constant_initializer(0.0))
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    with tf.variable_scope('layer2_pool1'):
+        pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('layer3_conv2'):
+        conv2_weights = tf.get_variable('weight', [CONV2_SIZE, CONV2_SIZE, NUM_CHANNELS, CONV2_DEEP], initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv2_biases = tf.get_variable('bias', [CONV2_DEEP], initializer=tf.constant_initializer(0.0))
+        conv2 = tf.nn.conv2d(pool1, conv2_weights, strides = [1, 1, 1, 1], padding='SAME')
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+        
+
+
+
