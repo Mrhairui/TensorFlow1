94608c143e5ff8634d7687e6c97f43a4d78454ef
Mrhairui <chenwentao_622@163.com>
2019-07-12 22:20:36 +0800

    tensorflow

--- mnist_train.py
+++ mnist_train.py
@@ -2,6 +2,8 @@ import os
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import mnist_inference
+import temp
+import numpy as np
 
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.8
@@ -15,10 +17,12 @@ MODEL_NAME = 'model.ckpt'
 
 
 def train(mnist):
-    x = tf.placeholder(tf.float32, [None, mnist_inference.INPUT_NODE], name='x_input')
-    y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y_put')
+#    x = tf.placeholder(tf.float32, [None, mnist_inference.INPUT_NODE], name='x_input')
+    x = tf.placeholder(tf.float32, [BATCH_SIZE, temp.IMAGE_SIZE, temp.IMAGE_SIZE, temp.NUM_CHANNELS], name='x_input')
+    y_ = tf.placeholder(tf.float32, [None, temp.OUTPUT_NODE], name='y_put')
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
-    y = mnist_inference.inference(x, regularizer)
+#    y = mnist_inference.inference(x, regularizer)
+    y = temp.inference(x, None, regularizer)
     global_step = tf.Variable(0, trainable=False)
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DACAY, global_step)
     variable_averages_op = variable_averages.apply(tf.trainable_variables())
@@ -35,7 +39,8 @@ def train(mnist):
         tf.global_variables_initializer().run()
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x:xs, y_:ys})
+            reshaped_xs = np.reshape(xs, (BATCH_SIZE, temp.IMAGE_SIZE, temp.IMAGE_SIZE, temp.NUM_CHANNELS))
+            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x:reshaped_xs, y_:ys})
             if i%1000 == 0:
                 print('after %d training step, loss on training''batcha is %g' %(step, loss_value))
                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
--- temp.py
+++ temp.py
@@ -23,7 +23,7 @@ def inference(input_tensor, train, regularizer):
         conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
 
-    with tf.variable_scope('layer2_pool1'):
+    with tf.name_scope('layer2_pool1'):
         pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
 
     with tf.variable_scope('layer3_conv2'):
@@ -31,7 +31,32 @@ def inference(input_tensor, train, regularizer):
         conv2_biases = tf.get_variable('bias', [CONV2_DEEP], initializer=tf.constant_initializer(0.0))
         conv2 = tf.nn.conv2d(pool1, conv2_weights, strides = [1, 1, 1, 1], padding='SAME')
         relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
-        
+
+    with tf.name_scope('layer4_pool2'):
+        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    pool_shape = pool2.get_shape().as_list()
+    nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+    reshaped = tf.reshape(pool2, [pool_shape[0], nodes])
+
+    with tf.variable_scope('layer5_fc1'):
+        fcl_weights = tf.get_variable('weights', [nodes, FC_SIZE], initializer = tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer is not None:
+            tf.add_to_collection('losses', regularizer(fcl_weights))
+        fcl_biases = tf.get_variable('bias', [FC_SIZE], initializer=tf.constant_initializer(0.1))
+        fc1 = tf.nn.relu(tf.matmul(reshaped, fcl_weights) + fcl_biases)
+        if train:
+            fc1 = tf.nn.dropout(fc1, 0.5)
+
+    with tf.variable_scope('layer6-fc2'):
+        fc2_weights = tf.get_variable('weights', [FC_SIZE, NUM_LABELS], initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer is not None:
+            tf.add_to_collection('losses', regularizer(fc2_weights))
+        fc2_biases = tf.get_variable('bias', [NUM_LABELS], tf.constant_initializer(0.1))
+        logit = tf.matmul(fc1, fc2_weights) + fc2_biases
+    return logit
+
+
+
 
 
 
